d1b6feff0911a70654aeca7137b4f57f1b510a89
suxuan0071 <1418649355@qq.com>
2021-12-15 16:35:21 +0800

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# -FleChAzo6
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  themeConfig: {
+    lastUpdated: 'Last Updated Time',
+    logo: '/assets/img/hero.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/about/' },
+      // { text: 'github', link: 'https://github.com/suxuan0071'},
+      {
+        text: 'git',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'github', link: 'https://github.com/suxuan0071' },
+          { text: 'gitee', link: 'https://gitee.com/zc0071' },
+          { text: 'csdn', link: 'https://blog.csdn.net/qq_42546585?spm=1000.2115.3001.5343' }
+
+        ]
+      }
+    ],
+  },
+  title: 'FleChAzo',
+  description: '61',
+}
