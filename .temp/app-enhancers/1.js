716c06e15e7cdaa4d62e44cc21d14c42c1a04be5
suxuan0071 <1418649355@qq.com>
2021-12-16 13:55:17 +0800

    github.io

---
+++ .temp/app-enhancers/1.js
@@ -0,0 +1,2 @@
+import "C:\\Users\\DELL\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\@vuepress\\plugin-nprogress\\enhanceAppFile.js"
+export default {}
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,17 +7,17 @@ module.exports = {
       { text: 'Guide', link: '/about/' },
       // { text: 'github', link: 'https://github.com/suxuan0071'},
       {
-        text: 'git',
-        ariaLabel: 'Language Menu',
+        text: 'Repositories||Blog',
+        ariaLabel: 'Repositories Menu',
         items: [
           { text: 'github', link: 'https://github.com/suxuan0071' },
           { text: 'gitee', link: 'https://gitee.com/zc0071' },
           { text: 'csdn', link: 'https://blog.csdn.net/qq_42546585?spm=1000.2115.3001.5343' }
-
         ]
       }
     ],
   },
   title: 'FleChAzo',
   description: '61',
+  base:"/-FleChAzo6/",
 }
